6cbafcca5fd2d7d87745e72d0d35a6b4ff1e8e43
Dragan Nikolic <dragan@fintechdev.net>
2017-02-03 15:04:57 -0800

    p15 more server examples

---
+++ 02_basic_server/server3-async-demo.js
@@ -0,0 +1,22 @@
+/*
+it demonstrate event driven nature of node
+
+CreateServer completes immediately without waiting for request,, and therefore
+'Server has started' is printed immediately.
+
+Then every time we enter localhost:8888 in our browser 'Request recevied; is
+printed on console (actuyally twice, not sure why?)
+*/
+
+var http = require("http");
+
+function onRequest(request, response) {     
+    console.log("Request received.");
+    response.writeHead(200, {"Content-Type": "text/plain"}); 
+    response.write("Hello World by Dragan Nikolic!");
+    response.end();
+}
+
+var server = http.createServer(onRequest).listen(8888);
+
+console.log("Server has started.");
