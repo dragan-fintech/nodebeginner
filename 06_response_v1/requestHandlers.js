ee7fffa683bfb97a1d738792eaea27849d07d70a
Dragan Nikolic <dragan@fintechdev.net>
2017-02-10 12:01:30 -0800

    add response v1 -- how not to do it

---
+++ 06_response_v1/requestHandlers.js
@@ -0,0 +1,12 @@
+// request_handlers.js
+
+function start() {
+    console.log("Request handler 'start' was called.");
+}
+
+function upload() {
+    console.log("Request handler 'upload' was called.");
+}
+
+exports.start = start;
+exports.upload = upload;
--- README.md
+++ README.md
@@ -3,5 +3,6 @@ The Node Beginner Book - A comprehensive Node.js tutorial
 
 progress: 
 
+20170210: p23
 20170205: p17
 20170202: p14
